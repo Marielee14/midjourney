3180ebc4b3a17c624214bbf04cc51ff091547e38
marie <marielee14@gmail.com>
2023-02-05 12:17:04 +0900

    create-post

--- src/components/FormField.jsx
+++ src/components/FormField.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
 
-const FormField = () => {
+const FormField = ({
+  labelName,
+  type,
+  name,
+  value,
+  placeholder,
+  handleChange,
+  isSurpriseMe,
+  handleSurpriseMe,
+}) => {
   return (
-    <div>FormField</div>
-  )
-}
+    <div>
+      <div className="flex items-center gap-2 mb-2">
+        <label
+          htmlFor={name}
+          className="block text-sm font-medium tex-gray-900">{labelName}</label>
+        {isSurpriseMe && (
+        <button
+          type="button"
+          onClick={handleSurpriseMe}
+          className="font-semibold text-xs bg-[#ececf1] py-1 px-2 rounded-[5px] text-black">
+          Surprise me
+        </button>
+        )}
+      </div>
+      <input
+        type={type}
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        required
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3"></input>
+    </div>
+  );
+};
 
-export default FormField
+export default FormField;
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,11 @@
 import { surpriseMePrompts } from "../constants";
 
 export function getRandomPrompt(prompt) {
+  //
   const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
   const getRandomPrompt = surpriseMePrompts[randomIndex];
 
+  //to make sure no redundancy
   if (getRandomPrompt === prompt) return getRandomPrompt(prompt);
   return getRandomPrompt;
 }
